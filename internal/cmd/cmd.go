455046f3c809cda0eb5bafd931fea9984e9ddc8f
Austin Valle <austinvalle@gmail.com>
2023-06-21 15:08:07 -0400

    quick refactor of existing code

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -5,12 +5,13 @@ package cmd
 
 import (
 	"context"
-	"log"
+	"flag"
+	"fmt"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-codegen-spec/spec"
 	"github.com/mitchellh/cli"
 
-	"github.com/hashicorp/terraform-plugin-codegen-framework/internal/config"
 	"github.com/hashicorp/terraform-plugin-codegen-framework/internal/datasource_convert"
 	"github.com/hashicorp/terraform-plugin-codegen-framework/internal/datasource_generate"
 	"github.com/hashicorp/terraform-plugin-codegen-framework/internal/format"
@@ -24,129 +25,207 @@ import (
 )
 
 type AllCommand struct {
-	UI cli.Ui
+	UI              cli.Ui
+	flagIRInputPath string
+	flagOutputPath  string
 }
 
-func (a AllCommand) Help() string {
-	return "Both -input and -output can be specified. " +
-		"-output defaults to output. "
+func (cmd *AllCommand) Flags() *flag.FlagSet {
+
+	fs := flag.NewFlagSet("all", flag.ExitOnError)
+	fs.StringVar(&cmd.flagIRInputPath, "input", "./ir.json", "path to intermediate representation (JSON)")
+	fs.StringVar(&cmd.flagOutputPath, "output", "./output", "directory path to output generated code files")
+
+	return fs
 }
 
-func (a AllCommand) Synopsis() string {
-	return "Generates schema."
+func (cmd *AllCommand) Help() string {
+	strBuilder := &strings.Builder{}
+
+	longestName := 0
+	longestUsage := 0
+	cmd.Flags().VisitAll(func(f *flag.Flag) {
+		if len(f.Name) > longestName {
+			longestName = len(f.Name)
+		}
+		if len(f.Usage) > longestUsage {
+			longestUsage = len(f.Usage)
+		}
+	})
+
+	strBuilder.WriteString("\nUsage: terraform-plugin-codegen-framework all [<args>]")
+	cmd.Flags().VisitAll(func(f *flag.Flag) {
+		if f.DefValue != "" {
+			strBuilder.WriteString(fmt.Sprintf("    --%s <ARG> %s%s%s  (default: %q)\n",
+				f.Name,
+				strings.Repeat(" ", longestName-len(f.Name)+2),
+				f.Usage,
+				strings.Repeat(" ", longestUsage-len(f.Usage)+2),
+				f.DefValue,
+			))
+		} else {
+			strBuilder.WriteString(fmt.Sprintf("    --%s <ARG> %s%s%s\n",
+				f.Name,
+				strings.Repeat(" ", longestName-len(f.Name)+2),
+				f.Usage,
+				strings.Repeat(" ", longestUsage-len(f.Usage)+2),
+			))
+		}
+	})
+	strBuilder.WriteString("\n")
+
+	return strBuilder.String()
 }
 
-func (a AllCommand) Run(args []string) int {
+func (a *AllCommand) Synopsis() string {
+	return "Generates Terraform Plugin Framework Go code files for a given Intermediate Representation (IR) JSON file."
+}
+
+func (cmd *AllCommand) Run(args []string) int {
 	ctx := context.Background()
-	// parse flags
-	conf, err := config.New(args)
+
+	fs := cmd.Flags()
+	err := fs.Parse(args)
 	if err != nil {
-		log.Fatal(err)
+		cmd.UI.Error(fmt.Sprintf("error parsing command flags: %s", err))
+		return 1
 	}
 
-	// read input file
-	src, err := input.Read(conf.Input)
+	err = cmd.runInternal(ctx)
 	if err != nil {
-		log.Fatal(err)
+		cmd.UI.Error(fmt.Sprintf("Error executing command: %s\n", err))
+		return 1
+	}
+
+	return 0
+}
+
+func (cmd *AllCommand) runInternal(ctx context.Context) error {
+	// read input file
+	src, err := input.Read(cmd.flagIRInputPath)
+	if err != nil {
+		return fmt.Errorf("error reading IR JSON: %w", err)
 	}
 
 	// validate JSON
 	err = validate.JSON(src)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("error validating IR JSON: %w", err)
 	}
 
+	// parse and validate IR against specification
 	spec, err := spec.Parse(ctx, src)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("error parsing IR JSON: %w", err)
 	}
 
-	// ********************
-	// DataSources
-	// ********************
+	// generate data sources
+	err = generateDataSourceCode(spec, cmd.flagOutputPath)
+	if err != nil {
+		return fmt.Errorf("error generating data source code: %w", err)
+	}
+
+	// generate resources
+	err = generateResourceCode(spec, cmd.flagOutputPath)
+	if err != nil {
+		return fmt.Errorf("error generating resource code: %w", err)
+	}
+
+	// generate provider
+	err = generateProviderCode(spec, cmd.flagOutputPath)
+	if err != nil {
+		return fmt.Errorf("error generating provider code: %w", err)
+	}
+
+	return nil
+}
+
+func generateDataSourceCode(spec spec.Specification, outputPath string) error {
 	// convert IR to framework schema
 	c := datasource_convert.NewConverter(spec)
 	schema, err := c.ToGeneratorDataSourceSchema()
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("error converting IR to Plugin Framework schema: %w", err)
 	}
 
 	// convert framework schema to []byte
 	g := datasource_generate.NewGeneratorDataSourceSchemas(schema)
 	schemaBytes, err := g.ToBytes()
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("error converting Plugin Framework schema to Go code: %w", err)
 	}
 
 	// format schema code
 	formattedDataSourcesSchema, err := format.Format(schemaBytes)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("error formatting Go code: %w", err)
 	}
 
 	// write code
-	err = output.WriteDataSources(formattedDataSourcesSchema, conf.Output)
+	err = output.WriteDataSources(formattedDataSourcesSchema, outputPath)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("error writing Go code to output: %w", err)
 	}
 
-	// ********************
-	// Resources
-	// ********************
+	return nil
+}
+
+func generateResourceCode(spec spec.Specification, outputPath string) error {
 	// convert IR to framework schema
 	resourceSchemaConverter := resource_convert.NewConverter(spec)
 	resourceSchemas, err := resourceSchemaConverter.ToGeneratorResourceSchema()
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("error converting IR to Plugin Framework schema: %w", err)
 	}
 
 	// convert framework schema to []byte
 	resourceSchemaGenerator := resource_generate.NewGeneratorResourceSchemas(resourceSchemas)
 	resourceSchemaBytes, err := resourceSchemaGenerator.ToBytes()
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("error converting Plugin Framework schema to Go code: %w", err)
 	}
 
 	// format schema code
 	formattedResourcesSchema, err := format.Format(resourceSchemaBytes)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("error formatting Go code: %w", err)
 	}
 
 	// write code
-	err = output.WriteResources(formattedResourcesSchema, conf.Output)
+	err = output.WriteResources(formattedResourcesSchema, outputPath)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("error writing Go code to output: %w", err)
 	}
 
-	// ********************
-	// Provider
-	// ********************
+	return nil
+}
+
+func generateProviderCode(spec spec.Specification, outputPath string) error {
 	// convert IR to framework schema
 	providerSchemaConverter := provider_convert.NewConverter(spec)
 	providerSchemas, err := providerSchemaConverter.ToGeneratorProviderSchema()
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("error converting IR to Plugin Framework schema: %w", err)
 	}
 
 	// convert framework schema to []byte
 	providerSchemaGenerator := provider_generate.NewGeneratorProviderSchemas(providerSchemas)
 	providerSchemaBytes, err := providerSchemaGenerator.ToBytes()
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("error converting Plugin Framework schema to Go code: %w", err)
 	}
 
 	// format schema code
 	formattedProvidersSchema, err := format.Format(providerSchemaBytes)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("error formatting Go code: %w", err)
 	}
 
 	// write code
-	err = output.WriteProviders(formattedProvidersSchema, conf.Output)
+	err = output.WriteProviders(formattedProvidersSchema, outputPath)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("error writing Go code to output: %w", err)
 	}
 
-	return 0
+	return nil
 }
