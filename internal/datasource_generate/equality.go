3ae0f4ad0f48aba31180c48660807c53cf559d83
Benjamin Bennett <ben.bennett@hashicorp.com>
2023-07-04 12:13:50 +0100

    Moving customTypeEqual(), elementTypeEqual() and objectTypeEqual() to equality.go (#25)

---
+++ internal/datasource_generate/equality.go
@@ -0,0 +1,143 @@
+package datasource_generate
+
+import specschema "github.com/hashicorp/terraform-plugin-codegen-spec/schema"
+
+func customTypeEqual(x, y *specschema.CustomType) bool {
+	if x == nil && y == nil {
+		return true
+	}
+
+	if x == nil && y != nil {
+		return false
+	}
+
+	if x != nil && y == nil {
+		return false
+	}
+
+	if x.Import == nil && y.Import != nil {
+		return false
+	}
+
+	if x.Import != nil && y.Import == nil {
+		return false
+	}
+
+	if x.Import != nil && y.Import != nil {
+		if *x.Import != *y.Import {
+			return false
+		}
+	}
+
+	if x.Type != y.Type {
+		return false
+	}
+
+	if x.ValueType != y.ValueType {
+		return false
+	}
+
+	return true
+}
+
+func elementTypeEqual(x, y specschema.ElementType) bool {
+	if x.Bool != nil && y.Bool != nil {
+		return customTypeEqual(x.Bool.CustomType, y.Bool.CustomType)
+	}
+
+	if x.Float64 != nil && y.Float64 != nil {
+		return customTypeEqual(x.Float64.CustomType, y.Float64.CustomType)
+	}
+
+	if x.Int64 != nil && y.Float64 != nil {
+		return customTypeEqual(x.Int64.CustomType, y.Int64.CustomType)
+	}
+
+	if x.List != nil && y.List != nil {
+		if !customTypeEqual(x.List.CustomType, y.List.CustomType) {
+			return false
+		}
+
+		return elementTypeEqual(x.List.ElementType, y.List.ElementType)
+	}
+
+	if x.Map != nil && y.Map != nil {
+		if !customTypeEqual(x.Map.CustomType, y.Map.CustomType) {
+			return false
+		}
+
+		return elementTypeEqual(x.Map.ElementType, y.Map.ElementType)
+	}
+
+	if x.Number != nil && y.Number != nil {
+		return customTypeEqual(x.Number.CustomType, y.Number.CustomType)
+	}
+
+	if x.Object != nil && y.Object != nil {
+		return objectTypeEqual(x.Object, y.Object)
+	}
+
+	if x.Set != nil && y.Set != nil {
+		if !customTypeEqual(x.Set.CustomType, y.Set.CustomType) {
+			return false
+		}
+
+		return elementTypeEqual(x.Set.ElementType, y.Set.ElementType)
+	}
+
+	if x.String != nil && y.String != nil {
+		return customTypeEqual(x.String.CustomType, y.String.CustomType)
+	}
+
+	return false
+}
+
+func objectTypeEqual(x, y *specschema.ObjectType) bool {
+	if x == nil && y == nil {
+		return true
+	}
+
+	if x == nil || y == nil {
+		return false
+	}
+
+	if !customTypeEqual(x.CustomType, y.CustomType) {
+		return false
+	}
+
+	for k, v := range x.AttributeTypes {
+		if v.Name != y.AttributeTypes[k].Name {
+			return false
+		}
+
+		a := specschema.ElementType{
+			Bool:    v.Bool,
+			Float64: v.Float64,
+			Int64:   v.Int64,
+			List:    v.List,
+			Map:     v.Map,
+			Number:  v.Number,
+			Object:  v.Object,
+			Set:     v.Set,
+			String:  v.String,
+		}
+
+		b := specschema.ElementType{
+			Bool:    y.AttributeTypes[k].Bool,
+			Float64: y.AttributeTypes[k].Float64,
+			Int64:   y.AttributeTypes[k].Int64,
+			List:    y.AttributeTypes[k].List,
+			Map:     y.AttributeTypes[k].Map,
+			Number:  y.AttributeTypes[k].Number,
+			Object:  y.AttributeTypes[k].Object,
+			Set:     y.AttributeTypes[k].Set,
+			String:  y.AttributeTypes[k].String,
+		}
+
+		if !elementTypeEqual(a, b) {
+			return false
+		}
+	}
+
+	return true
+}
