b287a779d69d3746fa90fab58b882721d351a795
Benjamin Bennett <ben.bennett@hashicorp.com>
2023-09-15 08:58:38 +0100

    Adding remaining equality checks for list, map, set, and single nested attributes, and list, set, and single nested blocks for data sources

--- internal/datasource_generate/types.go
+++ internal/datasource_generate/types.go
@@ -17,6 +17,28 @@ type GeneratorNestedAttributeObject struct {
 	Validators             specschema.ObjectValidators
 }
 
+func (g GeneratorNestedAttributeObject) Equal(other GeneratorNestedAttributeObject) bool {
+	for k := range g.Attributes {
+		if _, ok := other.Attributes[k]; !ok {
+			return false
+		}
+
+		if !g.Attributes[k].Equal(other.Attributes[k]) {
+			return false
+		}
+	}
+
+	if !g.AssociatedExternalType.Equal(other.AssociatedExternalType) {
+		return false
+	}
+
+	if !g.CustomType.Equal(other.CustomType) {
+		return false
+	}
+
+	return g.Validators.Equal(other.Validators)
+}
+
 type GeneratorNestedBlockObject struct {
 	Attributes schema.GeneratorAttributes
 	Blocks     schema.GeneratorBlocks
@@ -24,4 +46,36 @@ type GeneratorNestedBlockObject struct {
 	AssociatedExternalType *schema.AssocExtType
 	CustomType             *specschema.CustomType
 	Validators             specschema.ObjectValidators
+}
+
+func (g GeneratorNestedBlockObject) Equal(other GeneratorNestedBlockObject) bool {
+	for k := range g.Attributes {
+		if _, ok := other.Attributes[k]; !ok {
+			return false
+		}
+
+		if !g.Attributes[k].Equal(other.Attributes[k]) {
+			return false
+		}
+	}
+
+	for k := range g.Blocks {
+		if _, ok := other.Blocks[k]; !ok {
+			return false
+		}
+
+		if !g.Blocks[k].Equal(other.Blocks[k]) {
+			return false
+		}
+	}
+
+	if !g.AssociatedExternalType.Equal(other.AssociatedExternalType) {
+		return false
+	}
+
+	if !g.CustomType.Equal(other.CustomType) {
+		return false
+	}
+
+	return g.Validators.Equal(other.Validators)
 }
