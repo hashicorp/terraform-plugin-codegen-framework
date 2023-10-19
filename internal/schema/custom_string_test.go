71f0603103af7cd98f404f7762dd492d58c1cf7b
Benjamin Bennett <ben.bennett@hashicorp.com>
2023-10-19 07:59:42 +0100

    Adding tests for custom string type and value, and to/from methods (#70)

---
+++ internal/schema/custom_string_test.go
@@ -0,0 +1,460 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package schema
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestCustomStringType_renderEqual(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		name          string
+		expected      []byte
+		expectedError error
+	}{
+		"default": {
+			name: "Example",
+			expected: []byte(`func (t ExampleType) Equal(o attr.Type) bool {
+other, ok := o.(ExampleType)
+
+if !ok {
+return false
+}
+
+return t.StringType.Equal(other.StringType)
+}`),
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			customStringType := NewCustomStringType(testCase.name)
+
+			got, err := customStringType.renderEqual()
+
+			if diff := cmp.Diff(err, testCase.expectedError, equateErrorMessage); diff != "" {
+				t.Errorf("unexpected error: %s", diff)
+			}
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestCustomStringType_renderString(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		name          string
+		expected      []byte
+		expectedError error
+	}{
+		"default": {
+			name: "Example",
+			expected: []byte(`
+func (t ExampleType) String() string {
+return "ExampleType"
+}`),
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			customStringType := NewCustomStringType(testCase.name)
+
+			got, err := customStringType.renderString()
+
+			if diff := cmp.Diff(err, testCase.expectedError, equateErrorMessage); diff != "" {
+				t.Errorf("unexpected error: %s", diff)
+			}
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestCustomStringType_renderTypable(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		name          string
+		expected      []byte
+		expectedError error
+	}{
+		"default": {
+			name:     "Example",
+			expected: []byte(`var _ basetypes.StringTypable = ExampleType{}`),
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			customStringType := NewCustomStringType(testCase.name)
+
+			got, err := customStringType.renderTypable()
+
+			if diff := cmp.Diff(err, testCase.expectedError, equateErrorMessage); diff != "" {
+				t.Errorf("unexpected error: %s", diff)
+			}
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestCustomStringType_renderType(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		name          string
+		expected      []byte
+		expectedError error
+	}{
+		"default": {
+			name: "Example",
+			expected: []byte(`type ExampleType struct {
+basetypes.StringType
+}`),
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			customStringType := NewCustomStringType(testCase.name)
+
+			got, err := customStringType.renderType()
+
+			if diff := cmp.Diff(err, testCase.expectedError, equateErrorMessage); diff != "" {
+				t.Errorf("unexpected error: %s", diff)
+			}
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestCustomStringType_renderValueFromString(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		name          string
+		attrValues    map[string]string
+		expected      []byte
+		expectedError error
+	}{
+		"default": {
+			name: "Example",
+			attrValues: map[string]string{
+				"bool_attribute": "basetypes.StringValue",
+			},
+			expected: []byte(`
+func (t ExampleType) ValueFromString(ctx context.Context, in basetypes.StringValue) (basetypes.StringValuable, diag.Diagnostics) {
+return ExampleValue{
+StringValue: in,
+}, nil
+}`),
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			customStringType := NewCustomStringType(testCase.name)
+
+			got, err := customStringType.renderValueFromString()
+
+			if diff := cmp.Diff(err, testCase.expectedError, equateErrorMessage); diff != "" {
+				t.Errorf("unexpected error: %s", diff)
+			}
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestCustomStringType_renderValueFromTerraform(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		name          string
+		expected      []byte
+		expectedError error
+	}{
+		"default": {
+			name: "Example",
+			expected: []byte(`
+func (t ExampleType) ValueFromTerraform(ctx context.Context, in tftypes.Value) (attr.Value, error) {
+attrValue, err := t.StringType.ValueFromTerraform(ctx, in)
+
+if err != nil {
+return nil, err
+}
+
+boolValue, ok := attrValue.(basetypes.StringValue)
+
+if !ok {
+return nil, fmt.Errorf("unexpected value type of %T", attrValue)
+}
+
+boolValuable, diags := t.ValueFromString(ctx, boolValue)
+
+if diags.HasError() {
+return nil, fmt.Errorf("unexpected error converting StringValue to StringValuable: %v", diags)
+}
+
+return boolValuable, nil
+}`),
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			customStringType := NewCustomStringType(testCase.name)
+
+			got, err := customStringType.renderValueFromTerraform()
+
+			if diff := cmp.Diff(err, testCase.expectedError, equateErrorMessage); diff != "" {
+				t.Errorf("unexpected error: %s", diff)
+			}
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestCustomStringType_renderValueType(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		name          string
+		expected      []byte
+		expectedError error
+	}{
+		"default": {
+			name: "Example",
+			expected: []byte(`
+func (t ExampleType) ValueType(ctx context.Context) attr.Value {
+return ExampleValue{}
+}`),
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			customStringType := NewCustomStringType(testCase.name)
+
+			got, err := customStringType.renderValueType()
+
+			if diff := cmp.Diff(err, testCase.expectedError, equateErrorMessage); diff != "" {
+				t.Errorf("unexpected error: %s", diff)
+			}
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestCustomStringValue_renderEqual(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		name          string
+		attrValues    map[string]string
+		expected      []byte
+		expectedError error
+	}{
+		"default": {
+			name: "Example",
+			attrValues: map[string]string{
+				"bool_attribute": "basetypes.StringValue",
+			},
+			expected: []byte(`
+func (v ExampleValue) Equal(o attr.Value) bool {
+other, ok := o.(ExampleValue)
+
+if !ok {
+return false
+}
+
+return v.StringValue.Equal(other.StringValue)
+}`),
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			customStringValue := NewCustomStringValue(testCase.name)
+
+			got, err := customStringValue.renderEqual()
+
+			if diff := cmp.Diff(err, testCase.expectedError, equateErrorMessage); diff != "" {
+				t.Errorf("unexpected error: %s", diff)
+			}
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestCustomStringValue_renderType(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		name          string
+		expected      []byte
+		expectedError error
+	}{
+		"default": {
+			name: "Example",
+			expected: []byte(`
+func (v ExampleValue) Type(ctx context.Context) attr.Type {
+return ExampleType{
+}
+}`),
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			customStringValue := NewCustomStringValue(testCase.name)
+
+			got, err := customStringValue.renderType()
+
+			if diff := cmp.Diff(err, testCase.expectedError, equateErrorMessage); diff != "" {
+				t.Errorf("unexpected error: %s", diff)
+			}
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestCustomStringValue_renderValuable(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		name          string
+		expected      []byte
+		expectedError error
+	}{
+		"default": {
+			name:     "Example",
+			expected: []byte(`var _ basetypes.StringValuable = ExampleValue{}`),
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			customStringValue := NewCustomStringValue(testCase.name)
+
+			got, err := customStringValue.renderValuable()
+
+			if diff := cmp.Diff(err, testCase.expectedError, equateErrorMessage); diff != "" {
+				t.Errorf("unexpected error: %s", diff)
+			}
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestCustomStringValue_renderValue(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		name          string
+		expected      []byte
+		expectedError error
+	}{
+		"default": {
+			name: "Example",
+			expected: []byte(`type ExampleValue struct {
+basetypes.StringValue
+}`),
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			customStringValue := NewCustomStringValue(testCase.name)
+
+			got, err := customStringValue.renderValue()
+
+			if diff := cmp.Diff(err, testCase.expectedError, equateErrorMessage); diff != "" {
+				t.Errorf("unexpected error: %s", diff)
+			}
+
+			if diff := cmp.Diff(got, testCase.expected); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
